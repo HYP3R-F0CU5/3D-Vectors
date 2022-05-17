b947445fcbf5bc69caf2cf9a2b71430311057b3d
Shujan <gamesmagic2408@gmail.com>
2022-05-17 08:39:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ os.system("clear")
 a = input("Type in vector A from the top to the bottom leaving spaces between each number\n>")
 b = input("Do the same for vector B/n>")
 os.system("clear")
-while len(a.split()) != len(b.split()):
-        print("Both vectors must be the same length!")
-        a = input("Type in vector 'a' from the top to the bottom leaving spaces between each number\n>")
-        b = input("Do the same for vector b/n>")
 
 a = np.array(list(map(int, a.split())))
 b = np.array(list(map(int, b.split())))
@@ -27,8 +23,8 @@ elif menu == 2:
 	print("3. plane and a plane")
 	choice = int(input(">>>"))
 	if choice == 1:
-		print("The angle between the 2 lines are", str( np.arccos( (a @ b) / ( (sum([x**2 for x in a]) ** 0.5) * (sum([x**2 for x in b]) ** 0.5) ) ) ), "in Radians")
+		print("The angle between the 2 lines are:", str( np.arccos( round((a @ b) / ( (sum([x**2 for x in a]) ** 0.5) * (sum([x**2 for x in b]) ** 0.5) ), 3) )), "in Radians")
 	elif choice == 2:
-		print("The angle between the plane and the line is", str( np.arcsin( (a @ b) / ( (sum([x**2 for x in a]) ** 0.5) * (sum([x**2 for x in b]) ** 0.5) ) ) ), "in Radians")
+		print("The angle between the plane and the line is:", str( np.arcsin( round((a @ b) / ( (sum([x**2 for x in a]) ** 0.5) * (sum([x**2 for x in b]) ** 0.5) ), 3)) ), "in Radians")
 	else:
 		pass
